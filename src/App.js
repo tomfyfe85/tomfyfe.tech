55a6de88bbf5ea8253f1c42897a52532c947973a
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-05 01:26:30 +0100

    cont intergration test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        tomfyfe.tech
-        </p>
+        <p>tomfyfe.tech !!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
